938df9eef6abef2965960e11c5839b6860c9773e
Charles GBOYOU <gboyoucharles22@gmail.com>
2022-10-01 05:25:57 +0100

    Demande

--- resources/views/demandes/admindemandes.blade.php
+++ resources/views/demandes/admindemandes.blade.php
@@ -1,3 +1,8 @@
+<?php 
+namespace App\Http\Controllers; 
+use App\Models\Voiture;
+use App\Models\Chauffeur;
+?>
 @extends('master')
 @section('content')
 <main id="main" class="main">
@@ -8,17 +13,23 @@
                     <div class="col-12">
                         <div class="card recent-sales overflow-auto">
                             <div class="card-body">
-                                <h5 class="card-title">La liste des demandes</h5>
-
+                                <h5 class="card-title">Liste des demandes</h5>
+                                @if (session('status'))
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                        <i class="bi bi-check-circle me-1"></i>
+                                        {{ session('msg') }}
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>
+                                @endif
+                                <hr>
+                                @if( $demande->count() > 0 )
                                 <table class="table table-borderless datatable">
                                     <thead>
                                         <tr>
                                             <th scope="col">#</th>
-                                            <th scope="col">Objet de la demande</th>
-                                            <th scope="col">Voiture demandée</th>
-                                            <th scope="col">Chauffeur demandé</th>
-                                            <th scope="col">L'utilisateur qui a fait le demande</th>
-                                            <th scope="col">Sa structure</th>
+                                            <th scope="col">Objet De La Demande</th>
+                                            <th scope="col">Type</th>
+                                            <th scope="col">Demande</th>
                                             <th scope="col">Date début</th>
                                             <th scope="col">Date de fin</th>
                                             <th scope="col">Status</th>
@@ -26,29 +37,32 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @forelse ($demande as $item)
+                                        @foreach ($demande as $item)
                                             <tr>
                                                 <td>{{ $loop->iteration }}</td>
                                                 <td>{{ $item->objetdemande }}</td>
-                                                <td>{{ $item->voiture->marque }} ({{ $item->voiture->immatriculation }})</td>
+                                                <td><span class="text-primary">{{ ucfirst($item->type) }}</span></td>
                                                 <td>
-                                                    {{ $item->chauffeur->nom_cva ?? '--' }} {{ $item->chauffeur->prenom_cva ?? '--' }}
+                                                    @if( ($item->type == 'voiture') || $item->type == 'reparation' )
+                                                        <strong>{{ Voiture::find($item->affecter_id)->marque }} ( {{ Voiture::find($item->affecter_id)->immatriculation }} )</strong>
+                                                    @else
+                                                        <strong>{{ Chauffeur::find($item->affecter_id)->nom_cva }} {{ Chauffeur::find($item->affecter_id)->prenom_cva }} </strong>
+                                                    @endif
                                                 </td>
-                                                <td>{{ $item->user->name }}</td>
-                                                <td>{{ $item->user->structure->nomStructure }}</td>
                                                 <td>{{ $item->datedeb }}</td>
                                                 <td>{{ $item->datefin }}</td>
                                                 <td><span class="badge bg-danger">{{ $item->status }}</span></td>
-                                                <td><a class="btn btn-outline-danger btn-sm" href="">ok</a></td>
+                                                <td><a class="btn btn-outline-info btn-sm" href="">Valider</a></td>
                                             </tr>
-                                        @empty
-                                            <tr>Vous n'avez fait aucune demande !</tr>
-                                        @endforelse
+                                        @endforeach
                                     </tbody>
                                 </table>
-
+                                @else  
+                                    <div class="alert alert-warning">
+                                        Pas de demande !
+                                    </div>
+                                @endif
                             </div>
-
                         </div>
                     </div>
 
